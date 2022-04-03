e5073f9cc10b9119c6863175cfa7cce01b378b5f
Civitasv <2309780282@qq.com>
2022-04-03 11:09:43 +0800

    push latest

---
+++ ch15/exercises/04.c
@@ -0,0 +1,14 @@
+/**
+ * a. Output if DEBUG is defined:
+ *    Value of i: 1
+ *    Value of j: 2
+ *    Value of k: 3
+ *    Value of i + j: 3
+ *    Value of 2 * i + j - k: 1
+ *
+ * b. Output if DEBUG is not defined:
+ *
+ * c. DEBUG 未定义时，会进行判断，宏 PRINT_DEBUG 会被替换为空。
+ *
+ * d. 有必要。
+ */
